18c3d09fde7784ee60d34c6260e6885a17b859a0
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-09-06 18:52:57 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,34 @@
-import React from 'react';
-const Todo = ({dispatch,state}) => {
-   
-    return (
-        <div className='todo'>
-        {state.map((item)=> {
-            return(
-            <div className="title">
-                {item}
-                <button onClick={()=> dispatch({type:"DELETE",payload:{title:item.title,id:item.id}})} 
-                className="todo-delete">Delete</button>
-            </div>
-            )
-        })};
-        </div>
-    )
-}
+import React from "react";
+const Todo = ({ dispatch, state }) => {
+
+  console.log("state is ",state);
+  return (
+
+          <>
+            {state.map((item) => {
+              
+              return (
+                <div className="todo" key={item.id}> 
+                
+                  <div id={item.id} className="todo-title">
+                  {item.title}
+                  <button
+                    onClick={() =>
+                      dispatch({ type: "DELETE",payload:{title:item.title,id:item.id} })
+                    }
+                    className="todo-delete"
+                  >
+                    DELETE
+                  </button>
+                </div>
+                
+                </div>
+              );
+            })}
+            </>
+  );
+};
+
 
 
 export { Todo }
